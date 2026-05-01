e37046fcf2c5991a7d5c9eb9e3fee746f8406acf
nopeless <38830903+nopeless@users.noreply.github.com>
2026-05-01 04:25:22 -0500

    gh pages boilerplate yadiyada

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base:
+    process.env.GITHUB_ACTIONS === "true"
+      ? `/${process.env.GITHUB_REPOSITORY?.split("/")[1] ?? ""}/`
+      : "/",
   root: "src",
   build: {
     outDir: "../dist",
